1a1dbce65e9028d428e920e17e4d3e760c354c67
Matteo Di Giovinazzo <matteodg@gmail.com>
2023-12-27 20:58:37 +0100

    Initial Dart package created with Very Good CLI

---
+++ lib/src/tokencost.dart
@@ -0,0 +1,7 @@
+/// {@template tokencost}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class Tokencost {
+  /// {@macro tokencost}
+  const Tokencost();
+}
---
+++ test/src/tokencost_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:tokencost/tokencost.dart';
+
+void main() {
+  group('Tokencost', () {
+    test('can be instantiated', () {
+      expect(Tokencost(), isNotNull);
+    });
+  });
+}
